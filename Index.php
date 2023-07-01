0d472f345d27832293b131cfb54aa131d3370705
ANDREI UDEANU <udeanugheorgheandrei@gmail.com>
2023-07-01 23:02:25 +0200

    _Blank

---
+++ Index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Web Page</title>
+</head>
+<body>
+	<header>
+		<nav>
+            <button></button>
+        </nav>
+        <h1>The Space Agency</h1>
+	</header>
+	<main>
+		<section>
+            <h2>SCIENCE & EXPLORATION</h2>
+            <h1>Euclid liftoff</h1>
+            <p>01/07/2023 943 VIEWS 34 LIKES</p>
+            <button>PLAY</button>
+        </section>
+
+        <section>
+            <div>
+                <h2>ESA Vision</h2>
+                <div>
+                    <p>Agenda 2025</p>
+                    <p>Accelerators</p>
+                    <p>Advisory group report</p>
+                    <p>Revolution Space</p>
+                </div>
+            </div>
+            <div>
+                <h2>ESA Vision</h2>
+                <div>
+                    <p>Agenda 2025</p>
+                    <p>Accelerators</p>
+                    <p>Advisory group report</p>
+                    <p>Revolution Space</p>
+                </div>
+            </div>
+            <div>
+                <h2>ESA Vision</h2>
+                <div>
+                    <p>Agenda 2025</p>
+                    <p>Accelerators</p>
+                    <p>Advisory group report</p>
+                    <p>Revolution Space</p>
+                </div>
+            </div>
+            <div>
+                <h2>ESA Vision</h2>
+                <div>
+                    <p>Agenda 2025</p>
+                    <p>Accelerators</p>
+                    <p>Advisory group report</p>
+                    <p>Revolution Space</p>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div>
+                <h1>In the spotlight</h1>
+                <button>All</button>
+                <button>STORIES</button>
+                <button>VIDEOS</button>
+                <button>IMAGES</button>
+            </div>
+            <div>
+                <div>
+                    <h3></h3>
+                    <p></p>
+                    <button>VIEW</button>
+                </div>
+            </div>
+        </section>
+	</main>
+	<footer>
+		<p>© 2023 My Company</p>
+	</footer>
+</body>
+</html>
